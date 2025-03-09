e195c883f1d84d2469f6b87c78d4e829829d3c34
joshjones49 <joshjones200285@gmail.com>
2025-03-09 16:59:54 -0500

    `

--- index.mjs
+++ index.mjs
@@ -3,18 +3,6 @@ import * as route from './routeElements.mjs';
 import * as mp from './monthlyPayments.mjs'
 
 document.addEventListener('DOMContentLoaded', () => {
-    let container = '';
-
-    const updateDisplay = (page) => {
-        container = page
-        route.ci_page.style.display = container === 'CI' ? 'flex' : 'none';
-        route.home_page.style.display = container === 'HOME' ? 'flex' : 'none';
-        route.mp_page.style.display = container === 'MP' ? 'flex' : 'none';
-        route.roi_page.style.display = container === 'ROI' ? 'flex' : 'none';
-        route.npv_page.style.display = container === 'NPV' ? 'flex' : 'none';
-        route.irr_page.style.display = container === 'IRR' ? 'flex' : 'none';
-    }
-
     //==============USED TO SUBMIT, CALCULATE, AND CLEAR INPUTS
     ci.ci_submit.addEventListener('click', () => {
         ci.submitted ? ci.Clear() : ci.compoundInterest()
@@ -27,29 +15,29 @@ document.addEventListener('DOMContentLoaded', () => {
     //===============EVENT LISTENERS THAT CHANGE DISPLAYS
     route.home_page_btn.addEventListener('click', () => {
         console.log('HOME')
-        updateDisplay('HOME')
+        route.updateDisplay('HOME')
     })
 
     route.ci_page_btn.addEventListener('click', () => {
         console.log('CI')
-        updateDisplay('CI')
+        route.updateDisplay('CI')
     })
 
     route.mp_page_btn.addEventListener('click', () => {
         console.log('MP')
-        updateDisplay('MP')
+        route.updateDisplay('MP')
     })
 
     route.roi_page_btn.addEventListener('click', () => {
-        updateDisplay('ROI')
+        route.updateDisplay('ROI')
     })
 
     route.npv_page_btn.addEventListener('click', () => {
-        updateDisplay('NPV')
+        route.updateDisplay('NPV')
     })
 
     route.irr_page_btn.addEventListener('click', () => {
-        updateDisplay('IRR')
+        route.updateDisplay('IRR')
     })
 
 })
--- routeElements.mjs
+++ routeElements.mjs
@@ -21,3 +21,14 @@ export const npv_page_btn = document.querySelector('.npv-page-btn');
 //========================IRR ELEMENTS
 export const irr_page = document.querySelector('#irr-page');
 export const irr_page_btn = document.querySelector('.irr-page-btn');
+
+//========================FUNCTION TO UPDATE DISPLAY
+export const updateDisplay = (page) => {
+        let container = page
+        ci_page.style.display = container === 'CI' ? 'flex' : 'none';
+        home_page.style.display = container === 'HOME' ? 'flex' : 'none';
+        mp_page.style.display = container === 'MP' ? 'flex' : 'none';
+        roi_page.style.display = container === 'ROI' ? 'flex' : 'none';
+        npv_page.style.display = container === 'NPV' ? 'flex' : 'none';
+        irr_page.style.display = container === 'IRR' ? 'flex' : 'none';
+    }
