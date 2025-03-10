f9b9d3a0fe8c9b9f2830cff6d442e571c997584f
joshjones49 <joshjones200285@gmail.com>
2025-03-10 15:04:21 -0500

    continuing to create equation for NPV

--- netPresentValue.mjs
+++ netPresentValue.mjs
@@ -1,1 +1,23 @@
-export const npv = document.querySelector();
+export const npv_investment = document.querySelector('#npv-investment');
+export const npv_discount_rate = document.querySelector('#npv-discount-rate');
+export const npv_cashflows = document.querySelector('#npv-cashflows');
+export const npv_amount = document.querySelector('#npv-amount');
+export let submitted = false;
+
+export const netPresentValue = () => {
+    submitted = !submitted;
+
+    let investment = parseInt(npv_investment.value);
+    let rate = parseInt(npv_discount_rate.value) / 100;
+    let cashflow = npv_cashflows.value;
+    
+    let array = [];
+    cashflow = cashflow.split(',');
+    cashflow.forEach(number => {
+    number = parseInt(number)
+    array.push(number);
+    })
+
+    
+
+}
